1f1f1df8582dc338c0a82735c13e061f8a99ab7a
Gosols <teemu.a.kosonen@hotmail.com>
2020-04-20 15:58:06 +0300

    initial commit

---
+++ src/main/java/com/example/carshopbackend/CarshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.carshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshopBackendApplication.class, args);
+	}
+
+}
